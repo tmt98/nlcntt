2d6551a47c14ee37fd65ada2ea7d274d3c6cab2e
Tran Minh Tai <tranminhtai1998@gmail.com>
2020-05-18 19:36:35 +0700

    1805 1936

--- controllers/ajax.controller.js
+++ controllers/ajax.controller.js
@@ -2,16 +2,37 @@ var UserM = require('../models/user.model');
 var PostM = require('../models/post.model');
 
 module.exports.like = async (req, res) =>{
-    var idLike = req.signedCookies.id;
-    console.log(req.body);
-    let A = await PostM.findOne({_id : req.params.id })
-    // console.log(A);
-    return true;
+    // Biến ID
+    var ID_CMT = req.params.id;
+    var ID_POST =  req.body.idpost;
+    var ID_USER_LIKE = req.signedCookies.id;
+    console.log(ID_CMT);
+    // console.log(req.body)
+    // console.log(req.signedCookies.id);
+    // -->
+    // var LIKE = await PostM.updateOne({'_id': ID_POST,'comment._idcmt':ID_CMT}, { $push: {'comment.$.like':ID_USER_LIKE} })
+    var LIKE = await PostM.findOne({'_id': ID_POST,'comment._idcmt':ID_CMT})
+    console.log(LIKE);
+    res.send({
+        success: true,
+    });
 
 }
-module.exports.unlike = (req, res) =>{
-    // console.log('body: ' + JSON.stringify(req.body));
-    res.send("success");
-    return true;
+module.exports.unlike = async (req, res) =>{
+    // Biến ID
+    var ID_CMT = req.params.id;
+    var ID_POST =  req.body.idpost;
+    var ID_USER_LIKE = req.signedCookies.id;
+    console.log(ID_CMT);
+    // console.log(req.body)
+    // console.log(req.signedCookies.id);
+    // -->
+    var UNLIKE = await PostM.updateOne({'_id': ID_POST,'comment._idcmt':ID_CMT},
+        { $pullAll: {'comment.$.like': ID_USER_LIKE } }
+        )
+    console.log(UNLIKE);
+    res.send({
+        success: true,
+    });
 
 }
---
+++ models/comment.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const commentSchema =  new mongoose.Schema({
+    idpost: { type: mongoose.Schema.Types.ObjectID, ref: 'Post' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    datecmt: { type: Date, default: Date.now },
+    content: String,
+    like: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+});
+const Post = mongoose.model('Comment', commentSchema, 'comment');
+module.exports = Comment;
