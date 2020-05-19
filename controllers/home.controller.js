9dbe944b69888e990a69b566809ab32d00ad335f
Tran Minh Tai <tranminhtai1998@gmail.com>
2020-05-19 22:43:23 +0700

    Fixed 190520 2243

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,21 +1,30 @@
-var UserM = require('../models/user.model');
-var PostM = require('../models/post.model');
+var UserM = require("../models/user.model");
+var PostM = require("../models/post.model");
+var CommentM = require("../models/comment.model");
 module.exports.index = async (req, res) => {
-    var data = await PostM.find().populate('user').sort({datepost: -1})
-
-    // res.json(data);
-    res.render('home/index', {
-         posts: data
-    });
+  var data = await PostM.find().populate("user").sort({ datepost: -1 });
+  var countCMT = [];
+  // res.json(data);
+  for (let i = 0; i < data.length; i++) {
+    data[i].comment = await CommentM.count({ idpost: data[i]._id });
+    // console.log(data[i].comment);
+  }
+  console.log(data);
+  res.render("home/index", {
+    posts: data,
+  });
 };
 
 // Chưa fix
 module.exports.search = (req, res) => {
-    var q = req.query.q;
-    var matchedUsers = db.get('users').value().filter((user) => {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  var q = req.query.q;
+  var matchedUsers = db
+    .get("users")
+    .value()
+    .filter((user) => {
+      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
-    res.render('home/index',{
-        users: matchedUsers
-    });
-}
+  res.render("home/index", {
+    users: matchedUsers,
+  });
+};
--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,48 +1,80 @@
-var UserM = require('../models/user.model');
-var PostM = require('../models/post.model');
-var CommentM = require('../models/comment.model');
+var UserM = require("../models/user.model");
+var PostM = require("../models/post.model");
+var CommentM = require("../models/comment.model");
 
-module.exports.create = async (req,res) => {
-    var id = req.signedCookies.id;
-    var user = await UserM.findById(id);
-    console.log(user);
-    res.render('post/create',{
-        user: user
-    });
-}
-module.exports.createPOST = (req,res) => { // Tạo bài viết (Conntent)
-    console.log(req.signedCookies.id);
-    req.body.user = req.signedCookies.id;
-    req.body.tags = req.body.tags.split(',');
-    req.body.banner = "/" + req.file.destination + req.file.filename;
-    console.log(req.body);
-    PostM.create(req.body, function (err, PostM) {
-        if (err) return handleError(err);
-        // saved!
-        console.log(PostM.title + "da duoc them vao database")
-    });
-    res.redirect('/');
-}
+module.exports.create = async (req, res) => {
+  var id = req.signedCookies.id;
+  var user = await UserM.findById(id);
+  console.log(user);
+  res.render("post/create", {
+    user: user,
+  });
+};
+module.exports.createPOST = (req, res) => {
+  // Tạo bài viết (Conntent)
+  console.log(req.signedCookies.id);
+  req.body.user = req.signedCookies.id;
+  req.body.tags = req.body.tags.split(",");
+  req.body.banner = "/" + req.file.destination + req.file.filename;
+  console.log(req.body);
+  PostM.create(req.body, function (err, PostM) {
+    if (err) return handleError(err);
+    // saved!
+    console.log(PostM.title + "da duoc them vao database");
+  });
+  res.redirect("/");
+};
 // Test
 module.exports.upload = (req, res) => {
-	// Some operation
-	res.send({
-		"uploaded": 1,
-    	"fileName": "IMAGE.PNG",
-    	"url": "/public/img/user-upload"
-	})
-}
+  // Some operation
+  res.send({
+    uploaded: 1,
+    fileName: "IMAGE.PNG",
+    url: "/public/img/user-upload",
+  });
+};
 //
 module.exports.id = async (req, res) => {
-    var id = req.params.id;
-    var data = await (await PostM.findById(id).populate('user'));
-    var comment = await CommentM.find({idpost: id}).populate('user');
-    await PostM.updateOne({ _id: id }, {
-        view: data.view+1
-      });      
-    console.log(comment);
-    res.render('post/post-index', {
-        data: data,
-        comment: comment
-    });
-}
+  var id = req.params.id;
+  var checkLikeAr = [];
+  var data = await await PostM.findById(id).populate("user");
+  var comment = await CommentM.find({ idpost: id }, (err, comment) => {
+    // , like: {$in: req.signedCookies.id}
+    for (let i = 0; i < comment.length; i++) {
+      if (!req.signedCookies.id) {
+        checkLikeAr.push(false);
+      } else {
+        if (comment[i].like == []) {
+          checkLikeAr.push(false);
+        } else {
+          var checkLike = comment[i].like.find((a) => {
+            // console.log("CO LIKE HAY KHONG : " + a);
+            // console.log("ID LOGIN : " + req.signedCookies.id);
+            return a == req.signedCookies.id;
+          });
+          // console.log(checkLike);
+          if (checkLike == req.signedCookies.id) {
+            checkLikeAr.push(true);
+          } else {
+            checkLikeAr.push(false);
+          }
+        }
+      }
+    }
+  })
+    .populate("user")
+    .sort({ datecmt: -1 });
+  console.log(checkLikeAr);
+  await PostM.updateOne(
+    { _id: id },
+    {
+      view: data.view + 1,
+    }
+  );
+  console.log(comment);
+  res.render("post/post-index", {
+    data: data,
+    comment: comment,
+    checklike: checkLikeAr,
+  });
+};
