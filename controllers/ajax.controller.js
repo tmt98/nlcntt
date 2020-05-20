599ca1f39c357e7e96ab43c9c3928b6f777db769
Tran Minh Tai <tranminhtai1998@gmail.com>
2020-05-20 22:59:46 +0700

    200520 2259

--- controllers/ajax.controller.js
+++ controllers/ajax.controller.js
@@ -1,46 +1,62 @@
-var UserM = require('../models/user.model');
-var PostM = require('../models/post.model');
-var CommentM = require('../models/comment.model');
+var UserM = require("../models/user.model");
+var PostM = require("../models/post.model");
+var CommentM = require("../models/comment.model");
 
 module.exports.like = async (req, res) => {
-    // Biến ID
-    var ID_CMT = req.params.id;
-    var ID_POST =  req.body.idpost;
-    var ID_USER_LIKE = req.signedCookies.id;
-    console.log(ID_CMT + ":" + ID_POST + ":" + ID_USER_LIKE);
-    // -->
-    var LIKE = await CommentM.updateOne({'_id': ID_CMT}, { $push: {'like': ID_USER_LIKE}});
+  // Biến ID
+  var ID_CMT = req.params.id;
+  var ID_POST = req.body.idpost;
+  var ID_USER_LIKE = req.signedCookies.id;
+  console.log(ID_CMT + ":" + ID_POST + ":" + ID_USER_LIKE);
+  // -->
+  if (!req.signedCookies.id) {
+    res.redirect("/auth/login");
+  } else {
+    var LIKE = await CommentM.updateOne(
+      { _id: ID_CMT },
+      { $push: { like: ID_USER_LIKE } }
+    );
     console.log(LIKE);
     res.send({
-        success: true,
+      success: true,
     });
-}
+  }
+};
 module.exports.unlike = async (req, res) => {
-    // Biến ID
-    var ID_CMT = req.params.id;
-    var ID_POST =  req.body.idpost;
-    var ID_USER_LIKE = req.signedCookies.id;
-    console.log(ID_CMT + ":" + ID_POST + ":" + ID_USER_LIKE);
-    // -->
-    var UNLIKE = await CommentM.updateOne({'_id': ID_CMT}, { $pull: {'like': ID_USER_LIKE}});
+  // Biến ID
+  var ID_CMT = req.params.id;
+  var ID_POST = req.body.idpost;
+  var ID_USER_LIKE = req.signedCookies.id;
+  console.log(ID_CMT + ":" + ID_POST + ":" + ID_USER_LIKE);
+  // -->
+  if (!req.signedCookies.id) {
+    res.redirect("/auth/login");
+  } else {
+    var UNLIKE = await CommentM.updateOne(
+      { _id: ID_CMT },
+      { $pull: { like: ID_USER_LIKE } }
+    );
     console.log(UNLIKE);
     res.send({
-        success: true,
+      success: true,
     });
-}
+  }
+};
 
 module.exports.comment = async (req, res) => {
-    // Biến ID
-    var ID_POST =  req.body.idpost;
-    var ID_USER_CMT = req.signedCookies.id;
-    var CONTENT = req.body.value;
-    console.log(ID_POST+ ":" + ID_USER_CMT);
-    CommentM.create({idpost: ID_POST, user: ID_USER_CMT, content: CONTENT},function(err, CommentM){
-        if (err) return handleError(err);
-        console.log(CommentM._id + "success")
-    });
-    res.send({
-        success: true
-    });
-
-}
+  // Biến ID
+  var ID_POST = req.body.idpost;
+  var ID_USER_CMT = req.signedCookies.id;
+  var CONTENT = req.body.value;
+  console.log(ID_POST + ":" + ID_USER_CMT);
+  CommentM.create(
+    { idpost: ID_POST, user: ID_USER_CMT, content: CONTENT },
+    function (err, CommentM) {
+      if (err) return handleError(err);
+      console.log(CommentM._id + "success");
+    }
+  );
+  res.send({
+    success: true,
+  });
+};
--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,16 +2,16 @@ var UserM = require("../models/user.model");
 var PostM = require("../models/post.model");
 var CommentM = require("../models/comment.model");
 module.exports.index = async (req, res) => {
-  var data = await PostM.find().populate("user").sort({ datepost: -1 });
   var countCMT = [];
+  var data = await PostM.find().populate("user").sort({ datepost: -1 });
   // res.json(data);
   for (let i = 0; i < data.length; i++) {
-    data[i].comment = await CommentM.count({ idpost: data[i]._id });
-    // console.log(data[i].comment);
+    data[i].comment = await CommentM.countDocuments({ idpost: data[i]._id });
+    countCMT.push(data[i].comment);
   }
-  console.log(data);
   res.render("home/index", {
     posts: data,
+    countcmt: countCMT,
   });
 };
 
